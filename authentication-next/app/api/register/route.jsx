154a93ee38b41ae7cb4bcd7f796014ae09d7b5d3
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-10-01 10:07:56 +0300

    add to project api mongodbe

--- authentication-next/app/api/register/route.jsx
+++ authentication-next/app/api/register/route.jsx
@@ -1,19 +1,48 @@
+// import { connectMongoDB } from "@/app/lib/mongodb";
+// import { NextResponse } from "next/server";
+// // import User from "@/models/user";
+// import bcrypt from "bcryptjs"
+// import UserInfo from "@/app/components/UserInfo";
+
+// export async function POST(req) {
+//     try{
+//       const {name, email, password}= await req.json()
+//       const hashedPassword = await bcrypt.hash(password, 10)
+//         await connectMongoDB()
+//         // await User.create({name, email, password: hashedPassword})
+//         await UserInfo.create({name, email, password: hashedPassword})
+
+//       return NextResponse.json({message: "User registered successfully."},{status: 201})
+//     }catch(error){
+//         return NextResponse.json(
+//      {message: "An error occurred while registering the user."},{status: 500})
+//     }
+// }
+
+
+      ///////
+
+
+
+
+
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { connectMongoDB } from "@/app/lib/mongodb";
+import User from "@/app/models/user";
 
 export async function POST(req) {
-    try{
-      const {name, email, password}= await req.json()
+  try {
+    const { name, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB()
+    await User.create({ name, email, password: hashedPassword });
 
-      console.log("Name",name);
-      console.log("Email",email);
-      console.log("Password",password);
-
-      return NextResponse.json({message: "User registered successfully."},{status: 201})
-    }catch(error){
-        return NextResponse.json(
-     {message: "An error occurred while registering the user."},{status: 500})
-    }
+    return NextResponse.json({ message: "User registered." }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while registering the user." },
+      { status: 500 }
+    );
+  }
 }
-
-
-      
